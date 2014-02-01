515161c559451d6a56112e7760531f5a53a5b5f3
Mirtl <bubuzoglo@gmail.com>
2014-02-01 21:18:13 +0400

    uploaded

---
+++ test.js
@@ -0,0 +1,21 @@
+function start(){
+    debugger;
+    var xmlhttp, data, items;
+
+    if (window.XMLHttpRequest)
+    {// code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp=new XMLHttpRequest();
+    }
+    else
+    {// code for IE6, IE5
+        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.open("GET","xml/list.xml",true);
+    xmlhttp.send();
+    xmlhttp.onreadystatechange=function(){
+        if (xmlhttp.readyState==4 && xmlhttp.status==200){
+            data = xmlhttp.responseXML;
+            alert(data);
+        }
+    }
+}
